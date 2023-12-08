2ca1721cd532fd04dd7be0d869340a3cc64072b6
martinmh43 <145580847+martinmh43@users.noreply.github.com>
2023-12-08 12:40:17 +0100

    Creo que este esta bien
    He solucionado el problema del texto de una forma un poco xd la vd, pero ahora se pueden elegir todas las postales. Si pulsas en la T se quita el texto y si vuelves a pulsar se abre de nuevo, ademas lo he centrado que me parecia que estaba descolocado (al menos en mi pc) y he implementado un maximo de caracteres para que no se vaya de madres la cosa si escribes mucho

--- funciones.js
+++ funciones.js
@@ -5,3 +5,13 @@ function actualizarTexto() {
 
     document.getElementById("textoActualizado").value = inputTexto;
 }
+
+function cerrarTexto() {
+    var formulario = document.getElementById("miTexto");
+    formulario.style.display = "none";
+}
+
+function abrirTexto() {
+    var formulario = document.getElementById("miTexto");
+    formulario.style.display = "block";
+}
